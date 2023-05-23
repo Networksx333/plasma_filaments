0a039af681b7e0bb192c15de8b5ab8690a87e1e3
Networksx333 <127897894+Networksx333@users.noreply.github.com>
2023-05-23 23:51:07 +0300

    Add files via upload

--- plasma.py
+++ plasma.py
@@ -58,25 +58,30 @@ import zipfile
 
 """
 
-# # data = pd.read_table("plasma_filaments/filaments.dat", sep=" ", names=["t", "f"])
-# with open('plasma_filaments/filaments.dat', 'r') as f:
-#     lines = f.readlines()
-# print(lines[:10])
-# # удаляем первую строку
-# lines.pop(0)
+# data = pd.read_table("plasma_filaments/filaments.dat", sep=" ", names=["t", "f"])
 
-# # удаляем последние 4 строки
-# lines = lines[:-4]
+for i in os.listdir():
+    if i[-4:]==".dat":
+        file = i
 
-# # удаляем 4 пробела в начале каждой строки
-# lines = [line[4:] if line.startswith('    ') else line for line in lines]
+with open(file, 'r') as f:
+    lines = f.readlines()
+print(lines[:10])
+# удаляем первую строку
+lines.pop(0)
 
-# # заменяем двойные пробелы на одинарные
-# lines = [line.replace('  ', ' ') for line in lines]
+# удаляем последние 4 строки
+lines = lines[:-4]
 
-# with open('filaments.dat', 'w') as f:
-#     f.writelines(lines)
-data = pd.read_table("plasma_filaments/filaments.dat", sep=" ", names=["t", "f"])
+# удаляем 4 пробела в начале каждой строки
+lines = [line[4:] if line.startswith('    ') else line for line in lines]
+
+# заменяем двойные пробелы на одинарные
+lines = [line.replace('  ', ' ') for line in lines]
+
+with open('fil.dat', 'w') as f:
+    f.writelines(lines)
+data = pd.read_table("fil.dat", sep=" ", names=["t", "f"])
 data.astype({"t": "float64"})
 
 plt.figure(figsize=(10, 5))
@@ -290,8 +295,6 @@ with open("plasma_filaments/data_filter.txt", "r") as f:
   fil_for_training = np.array(fil_for_training).astype(np.float64)
   answers = np.array(answers).reshape(-1, 1).astype(np.float64)
 
-!rm -rf untitled_project
-
 def find_key_with_text(dictionary, text):
     for key in dictionary.keys():
         if text in key:
@@ -339,9 +342,6 @@ scaler = joblib.load("plasma_filaments/scaler_for_neuro_filter.pkl")
 
 filtered = neuro_filter.predict(scaler.transform(filaments[1])) > 0.8
 
-trajectory = keras.models.load_model("trajectory_model.h5",
-                                     custom_objects={'TCN':TCN})
-
 count = 0
 
 for i in range(len(filaments[0])):
@@ -383,174 +383,3 @@ print(count)
 # print(f"Количество найдённых филаментов {count}, по файлу А.Ю. их должно быть {filr.shape[0]} ")
 
 make_archive("data", "zip", "plasma_filaments/data")
-
-"""## Матрица филаментов"""
-
-# Указание названия архива
-archive = 'plasma_filaments/M.zip'
-
-# Открытие и распаковка архива
-with zipfile.ZipFile(archive, 'r') as zip_file:
-  zip_file.extractall()
-
-import seaborn as sns
-# Bresenham Line
-def bresenham_line(matrix, i1, j1, i2, j2):
-    result = []
-    dx = abs(j2 - j1)
-    dy = abs(i2 - i1)
-    sx = 1 if j1 < j2 else -1
-    sy = 1 if i1 < i2 else -1
-    err = dx - dy
-    while True:
-        result.append(matrix[i1][j1])
-        if j1 == j2 and i1 == i2:
-            break
-        e2 = 2 * err
-        if e2 > -dy:
-            err -= dy
-            j1 += sx
-        if e2 < dx:
-            err += dx
-            i1 += sy
-    return result
-
-
-# Set the number of files
-n = 6000
-
-# Set freq
-
-f = 29*10**9
-
-# Making an array of positions with id
-with open("M/positions.txt", "r") as positions:
-  lines = positions.readlines()
-
-lines = lines[2:]
-lines = [line.replace("\n", "").split(" ") for line in lines]
-lines = [[round(float(line[0])), float(line[1]), float(line[2])] 
-         for line in lines]
-positions = pd.DataFrame(lines, columns=["id", "r", "z"])
-
-# Create list with ids
-ids = []
-for i in range(n):
-  index = str(i)
-  name = "M/globus_" + "0"*(4 - len(index)) + index + ".txt"
-  with open(name, "r") as fi:
-    lines = fi.readlines()
-  lines = lines[2:]
-  lines = [line.split(" ")[:-1] for line in lines]
-  lines = [[float(line[0]), float(line[1])] 
-         for line in lines]
-  ids.append(lines)
-
-# Making plot of the positions
-x = sorted(list(set(positions.r)))
-y = sorted(list(set(positions.z)))
-
-# Creating a variable for the matrix
-M = np.zeros((len(y), len(x)))
-
-x_, y_ = np.meshgrid(x, y)
-
-t = 0
-for r, z in zip(np.hstack(x_), np.hstack(y_)):
-    # print(r, z, sep="\n")
-    id = positions.loc[(positions["r"] == r) & \
-                       (positions["z"] == z), 'id'].iloc[0]
-    for j in range(len(ids[id])):
-      if abs(ids[id][j][0] - f) < 10:
-        M[t//M.shape[1]][t%M.shape[1]] = ids[id][j][1]
-    t += 1
-
-M = M - (np.abs(M) > 1)*M
-M = np.round(M, 4)
-
-# Make a line
-x0 = 0
-y0 = 42
-x1 = 99
-y1 = 42
-h = 1
-
-k = 1
-for i in range(k):
-  y1 += h
-  y0 = y1
-  sns.set()
-  fig, ax = plt.subplots(1, 2, figsize=(13,5))
-  sns.heatmap(ax=ax[0], data=M, norm=Normalize())
-  ax[0].plot([y0, y1], [x0, x1], color="white", linewidth=2)
-  sns.lineplot(ax=ax[1], data=bresenham_line(M, x0, y0, x1, y1))
-  # plt.savefig(f"video/{i}.png")
-  plt.show()
-  clear_output(wait=True)
-
-x0 = 0
-y0 = 0
-x1 = 99
-y1 = 0
-h = 1
-
-# fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-# shape = cv2.imread("video/" + str(1) + '.png').shape
-# video = cv2.VideoWriter('video.mp4', fourcc, 3, (shape[1], shape[0]))
-
-# for j in range(23*2+22+23+22):
-#     img = cv2.imread("video/" + str(j) + '.png')
-#     video.write(img)
-
-# cv2.destroyAllWindows()
-# video.release()
-
-# Make dataset
-X_M = []
-Y_M = []
-
-for y0 in range(33, 60):
-  for y1 in range(33, 60):
-    arr = bresenham_line(M, 0, y0, 99, y1)
-    arr = np.interp(np.linspace(0, len(arr) - 1, 64), np.arange(len(arr)), arr)
-    X_M.append(arr)
-    Y_M.append([y0, y1])
-X_M = np.array(X_M)
-Y_M = np.array(Y_M)
-
-# Neural Network
-
-m_train, m_test, answ_m_train, answ_m_test = train_test_split(X_M, 
-                                                              Y_M,
-                                                          test_size=0.3)
-
-m_scaler = StandardScaler()
-
-m_train = m_scaler.fit_transform(m_train)
-m_test = m_scaler.transform(m_test)
-joblib.dump(m_scaler, "scaler_for_matrix.pkl")
-input_layer = Input(shape=(64, 1))
-x = TCN(nb_filters=64, kernel_size=5, dilations=[1, 2, 4, 8], return_sequences=True)(input_layer)
-x = TCN(nb_filters=32, kernel_size=5, dilations=[1, 2, 4, 8], return_sequences=True)(x)
-x = TCN(nb_filters=16, kernel_size=5, dilations=[1, 2, 4, 8])(x)
-x = Dense(32, activation='relu')(x)
-x = Dense(16, activation='relu')(x)
-output_layer = Dense(2, activation='linear')(x)
-
-model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
-
-model.fit(m_train, answ_m_train, epochs=20)
-model.evaluate(m_test, answ_m_test)
-model.save("trajectory_model.h5")
-
-def preprocess(x):
-  x = np.interp(np.linspace(0, len(x) - 1, 64), np.arange(len(x)), x)
-  x = m_scaler.transform(np.array([x]))
-  return x
-
-up, down = 0, 99
-test_object = preprocess(bresenham_line(M, up, 38, down, 55)).reshape(-1, 64)
-
-model.predict(test_object)
-
